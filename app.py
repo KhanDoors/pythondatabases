a4d551e4c1a377e51be28bab6179e738ac78b0b1
Syyad Khan <syykhan@deloitte.com>
2019-09-15 18:36:09 -0400

    final commit for today

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from user import User
+from database import Database
 
-my_user = User.load_from_db_by_email('cowboys@cowboys.com')
+Database.initialise(database="learning", user="postgres", password="syyad", host="localhost")
 
-my_user.save_to_db()
+user = User('syyad@syyad.com', 'syyad', 'khan')
 
+user.save_to_db()
+
+user_from_db = User.load_from_db_by_email('syyad@syyad.com')
+
+print(user_from_db)
